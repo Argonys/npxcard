144e4a4300d9bc58375a3b22e0c37e8100e27b75
Sirlys <alicia.norga@gmail.com>
2020-05-08 10:52:37 +0200

    npx card finished

---
+++ index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const data = {
+    name: chalk.yellowBright("             Alicia Norga"),
+    handle: chalk.blueBright.inverse(" Lyzzia "),
+    work: chalk.blueBright("BeCode Student"),
+    npm: chalk.white("https://www.npmjs.com/") + chalk.blueBright("~lyzzia"),
+    slash: chalk.white("/"),
+    github: chalk.white("https://github.com/") + chalk.blueBright("sirlys"),
+    linkedin:
+        chalk.white("https://linkedin.com/in/") +
+        chalk.blueBright("alicia-norga-1243b619b/"),
+    npx: chalk.blueBright("npx") + " " + chalk.yellowBright("lyzzia"),
+    labelWork: chalk.white.bold("       Work:"),
+    labelNpm: chalk.white.bold("        npm:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelCard: chalk.white.bold("       Card:"),
+    draw1: "•",
+    draw2: chalk.yellowBright("›×‹"),
+};
+
+const NEWLINE = "\n";
+const EMPTYLINE = "";
+
+console.log(
+    chalk.blueBright(
+        boxen(
+            [
+                `${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2}`,
+                EMPTYLINE,
+                EMPTYLINE,
+                `${data.name} ${data.slash} ${data.handle}`,
+                EMPTYLINE,
+                `${data.labelWork} ${data.work}`,
+                EMPTYLINE,
+                `${data.labelNpm} ${data.npm}`,
+                `${data.labelGitHub} ${data.github}`,
+                `${data.labelLinkedIn} ${data.linkedin}`,
+                EMPTYLINE,
+                `${data.labelCard} ${data.npx}`,
+                EMPTYLINE,
+                EMPTYLINE,
+                `${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2} ${data.draw1} ${data.draw2}`,
+            ].join(NEWLINE),
+            {
+                padding: 1,
+                margin: 2,
+                borderStyle: "double",
+            }
+        )
+    )
+);
+qzdz;
+
+qdqzd;
+zqzdqzdqzqzdqzd;
+qdzd;
+qzdzq;
+qzdqzdzqd;
+hgvkjhk;
+yfyt;
+esr;
+trd;
+uygiftu;
+rytdf;
